2f0d17d9f89ede2d2fdd7f69646481cea410ccef
Akash <95273877+B-R-Akash@users.noreply.github.com>
2021-11-30 23:16:12 +0530

    Delete application/src/example directory

--- application/src/example/example.kt
+++
@@ -1,8 +0,0 @@
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-
