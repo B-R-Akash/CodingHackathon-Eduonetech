5ddb38809ae54bb57bb7f5f16a57dfa67a62d9fc
Akash <95273877+B-R-Akash@users.noreply.github.com>
2021-11-30 23:17:19 +0530

    Create example.kt

---
+++ application/src/example.kt
@@ -0,0 +1,7 @@
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
